e50012ff61f46b99c2e62926e6aecf873945579b
fare333 <farisganija022@gmail.com>
2023-01-11 12:13:51 +0100

    update commit

--- profiles/urls.py
+++ profiles/urls.py
@@ -4,11 +4,11 @@ from . import views
 
 urlpatterns = [
 	path("my_profile/",views.my_profile,name="my_profile"),
- 	path("<str:pk>/",views.get_profiles,name="get_profiles"),
+ 	path("p/<str:pk>/",views.get_profiles,name="get_profiles"),
     path("search",views.search,name="search"),
     path("add-follow/<str:pk>/",views.add_follow,name="add_follow"),
-    path("p/get_all_profiles/",views.get_all_profiles,name="all_profiles"),
-    path("p/edit_profile/",views.edit_profile,name="edit_profile"),
-    path("p/all_user_followers",views.all_user_followers,name="all_user_followers"),
-    path("p/user_followers/<str:pk>/",views.user_followers,name="user_followers")
+    path("get_all_profiles/",views.get_all_profiles,name="all_profiles"),
+    path("edit_profile/",views.edit_profile,name="edit_profile"),
+    path("all_user_followers",views.all_user_followers,name="all_user_followers"),
+    path("user_followers/<str:pk>/",views.user_followers,name="user_followers")
 ]
